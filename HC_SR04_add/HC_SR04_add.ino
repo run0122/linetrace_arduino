3df46e0cc3a9af9d2d184261e56486f749605eae
RedCloud79 <ghddns79@naver.com>
2023-04-13 17:11:53 +0900

    Update HC_SR04_add.ino

--- HC_SR04_add/HC_SR04_add.ino
+++ HC_SR04_add/HC_SR04_add.ino
@@ -4,6 +4,9 @@ const int motor_B1 = 9;
 const int motor_B2 = 10;
 const int trigPin = 3;
 const int echoPin = 11;
+const int IR_R = A1;
+const int IR_M = A3;
+const int IR_L = A5;
 int IR_L_data;
 int IR_M_data;
 int IR_R_data;
@@ -18,12 +21,19 @@ void setup() {
   pinMode(motor_B2, OUTPUT);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+  pinMode(IR_L, INPUT);
+  pinMode(IR_M, INPUT);
+  pinMode(IR_R, INPUT);
   //Serial.begin(9600);
   //Serial.print("Start");
 }
 
 
 void loop() {
+  IR_L_data = digitalRead(IR_L);
+  IR_M_data = digitalRead(IR_M);
+  IR_R_data = digitalRead(IR_R);
+  
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
@@ -35,42 +45,44 @@ void loop() {
   //Serial.print("Distance: ");
   //Serial.println(distance);
   delay(100);
-
-  if(distance > 10){
-    rightback ();
-  }else{
+  if(distance > 15 && distance < 1023){
     if (IR_L_data == 0 and IR_M_data == 1 and IR_R_data == 0) {
       //Serial.println(" 직진 ");
       forward();
-      delay(100);
+      //delay(100);
     }
     else if (IR_L_data == 1 and IR_M_data == 0 and IR_R_data == 0) {
       //Serial.println(" 좌회전 ");
       left ();
-      delay(100);
+      //delay(100);
     }
     else if (IR_L_data == 1 and IR_M_data == 1 and IR_R_data == 0) {
       //Serial.println(" 좌회전 ");
       left ();
-      delay(100);
+      //delay(100);
     }
     else if (IR_L_data == 0 and IR_M_data == 0 and IR_R_data == 1) {
       //Serial.println(" 우회전 ");
       right ();
-      delay(100);
+      //delay(100);
     }
     else if (IR_L_data == 0 and IR_M_data == 1 and IR_R_data == 1) {
       //Serial.println(" 우회전 ");
       right ();
-      delay(100);
+      //delay(100);
     }
 
     else if (IR_L_data == 1 and IR_M_data == 1  and IR_R_data == 1) {
       //Serial.println(" 정지 ");
       stop();
-      delay(100);
+      //delay(100);
     }
   }
+  if(distance > 0 && distance < 15){
+      rightback ();
+      delay(3000);
+      stop();
+      }
 }
 
 
