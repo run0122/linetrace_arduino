95556c1c14dd647479c61473938bd5addb0f222e
run0122 <39726423+run0122@users.noreply.github.com>
2023-04-28 15:33:25 +0900

    Update Yolov5_people_follow.py

--- Yolov5_people_follow.py
+++ Yolov5_people_follow.py
@@ -14,9 +14,6 @@ while True:
         break
     results = model(frame)  # 객체 감지 수행
 
-    # 결과를 처리합니다.
-    cx_list = []
-    cy_list = []
     for detection in results.xyxy[0]:
         # 객체의 위치와 클래스 정보를 가져옵니다.
         x1, y1, x2, y2, conf, cls = detection
@@ -33,57 +30,55 @@ while True:
             # 객체의 중심점(cx, cy) 계산
             cx = int((x1 + x2) / 2)
             cy = int((y1 + y2) / 2)
-            cx_list.append(cx)
-            cy_list.append(cy)
 
             cv2.circle(frame, (cx, cy), 5, (0, 0, 255), -1)
 
-    # 여러 명의 person 객체의 중심점 평균 계산
-    if len(cx_list) > 0 and len(cy_list) > 0:
-        avg_cx = int(np.mean(cx_list))
-        avg_cy = int(np.mean(cy_list))
+            if cx < 210:
+                ser.write(b'R')
+            elif cy > 450:
+                ser.write(b'L')
+            else:
+                ser.write(b'B')
 
-        # 중심점 정보 시리얼 통신으로 전송하기
-        if avg_cx < 160:
-            ser.write(b'MR')
-        elif avg_cx > 480:
-            ser.write(b'ML')
-        else:
-            ser.write(b'F')
+            cv2.circle(frame, (cx, cy), 5, (255, 0, 0), -1)
 
-        cv2.circle(frame, (avg_cx, avg_cy), 5, (255, 0, 0), -1)
-
-        print("Average Center point: ({}, {})".format(avg_cx, avg_cy))
+            print("Average Center point: ({}, {})".format(cx, cy))
 
     cv2.imshow('YOLOv5', frame)  # 화면에 프레임을 출력합니다.
 
     height, width, channels = frame.shape
 
     # 하단부 중앙 1/2 영역 추출하기
-    crop_width = int(width / 4)
+    crop_width = int(width * 2 / 3)  # 가로 크기를 2배로 늘림
     crop_height = int(height / 2)
-    start_x = int(width / 2) - crop_width
+    start_x = 0
     start_y = int(height / 2)
-    end_x = int(width / 2) + crop_width
+    end_x = width
     end_y = height
+
+    # 이미지 경계를 벗어나는 값을 사용하지 않도록 주의해야 합니다.
+    if end_x > width:
+        end_x = width
+
     cropped_frame = frame[start_y:end_y, start_x:end_x]
 
     # 그레이스케일 이미지로 변환하기
     gray = cv2.cvtColor(cropped_frame, cv2.COLOR_BGR2GRAY)
 
     # 이미지 이진화하기
-    _, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY_INV)
+    _, thresh = cv2.threshold(gray, 80, 255, cv2.THRESH_BINARY_INV)
 
     # 컨투어 찾기
-    contours, _ = cv2.findContours(
-        thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
+    # contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     # 컨투어 중 가장 큰 영역 찾기
+
     max_area = 0
+    min_contour_area = 10000
     max_contour = None
     for contour in contours:
         area = cv2.contourArea(contour)
-        if area > max_area:
+        if area > max_area and area > min_contour_area:
             max_area = area
             max_contour = contour
 
@@ -98,9 +93,9 @@ while True:
             cv2.circle(thresh, (cx, cy), 5, (0, 0, 255), -1)
 
             # 중심점 정보 시리얼 통신으로 전송하기
-            if cx < 160:
+            if cx < 210:
                 ser.write(b'L')
-            elif cx > 480:
+            elif cx > 450:
                 ser.write(b'R')
 
             else:
