d456364b2b5bc1939fefbbaeb4405fee24bef3f6
RedCloud79 <ghddns79@naver.com>
2023-04-28 10:54:58 +0900

    add

--- webcam_move_people/webcam_move_people.ino
+++ webcam_move_people/webcam_move_people.ino
@@ -3,6 +3,8 @@ int in2 = 6;
 int in3 = 9;
 int in4 = 10;
 
+bool is_turned = false;
+
 void setup() {
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
@@ -17,19 +19,23 @@ void loop() {
 
     if (input == 'F') {
       forward();
-      delay(20);
+      delay(80);
       stop();
     } else if (input == 'B') {
       backward();
-      delay(20);
+      delay(80);
       stop();
-    } else if (input == 'L') {
+    } else if (input == 'T'){
+      trun_1();
+    } else if (input == 'J'){
+      trun_2();
+    }else if (input == 'L') {
       left();
-      delay(20);
+      delay(80);
       stop();
-    }else if (input == 'R') {
+    } else if (input == 'R') {
       right();
-      delay(20);
+      delay(80);
       stop();
     } else {
       stop();
@@ -71,4 +77,72 @@ void stop() {
   digitalWrite(in2, LOW);
   digitalWrite(in3, LOW);
   digitalWrite(in4, LOW);
+}
+
+void turn_1() {
+  //왼쪽으로 우회
+  millifirstleft(500);
+  milliforward(700);
+  milliright(500);
+  milliforward(700);
+  milliright(500);
+  milliforward(700);
+  //is_turned = true;
+}
+
+void turn_2() {
+  //오른쪽으로 우회
+  milliright(500);
+  milliforward(1500);
+  millifirstleft(500);
+  milliforward(1500);
+  millifirstleft(500);
+  milliforward(1500);
+  //is_turned = true;
+}
+
+void millifirstleft(unsigned long x) {
+  unsigned long current_time = millis();
+  unsigned long interval = x;
+  while (millis() - current_time < interval) {
+    hardleft();
+  }
+}
+void milliright(unsigned long x) {
+  unsigned long current_time = millis();
+  unsigned long interval = x;
+  while (millis() - current_time < interval) {
+    hardright();
+  }
+}
+void milliright(unsigned long x) {
+  unsigned long current_time = millis();
+  unsigned long interval = x;
+  while (millis() - current_time < interval) {
+    hardforward();
+  }
+}
+
+void hardright() {
+  //우
+  digitalWrite(motor_A1, HIGH);
+  digitalWrite(motor_A2, LOW);
+  digitalWrite(motor_B1, LOW);
+  digitalWrite(motor_B2, HIGH);
+}
+
+void hardleft() {
+  //좌
+  digitalWrite(motor_A1, LOW);
+  digitalWrite(motor_A2, HIGH);
+  digitalWrite(motor_B1, HIGH);
+  digitalWrite(motor_B2, LOW);
+}
+
+void hardforward() {
+  //직진
+  digitalWrite(motor_A1, HIGH);
+  digitalWrite(motor_A2, LOW);
+  digitalWrite(motor_B1, HIGH);
+  digitalWrite(motor_B2, LOW);
 }
